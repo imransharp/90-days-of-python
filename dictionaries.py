ce27c015a49f302a98f87a66eef9282ef50d37ae
Imran Bhatti <imranb.bhatti@gmail.com>
2025-03-03 08:23:00 +0500

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+# person , age, name, city,
+
+person = {"name": "imran", "age": "40", "city": "Islamabad", "Country": "Pakistan"}
+print(person["name"])
+print(person["age"])
+print(person["city"])
